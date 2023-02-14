3de9a06581eeb8ff1d3596d759d9c455532ee483
Tim Kim <timk@adobe.com>
2023-02-14 14:16:41 -0800

    Update path

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@
 module.exports = {
   // pathPrefix: process.env.PATH_PREFIX || "/creative-cloud/distribution/",
   // The /developer-distribution will go to the helix landing page
-  pathPrefix: process.env.PATH_PREFIX || "/developer-distribution/overview",
+  pathPrefix: process.env.PATH_PREFIX || "/developer-distribution/docs/",
   siteMetadata: {
     pages: [
       {
